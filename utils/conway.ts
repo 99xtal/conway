8c69dd2d0d1e8d1901f6db2781916c65004215a6
Joe Rybarczyk <joerybarczyk99@gmail.com>
2023-01-26 00:30:17 -0500

    feat: better algorithm

--- utils/conway.ts
+++ utils/conway.ts
@@ -1,66 +1,45 @@
-import { Coordinate as c } from '../types';
 
-enum CellState {
-  'DEAD' = 0,
-  'ALIVE' = 1,
-}
-
-// Key for cell state map
-type Coordinate = `${number},${number}`;
-
-type CellMap = Map<Coordinate, CellState>;
-
-const testCells: CellMap = new Map([
-  ['-1,0', 1],
-  ['0,0', 1],
-  ['1,0', 1],
-]);
-
-function getNeighbors(coords: Coordinate): Coordinate[] {
-  const relNeighbors = [
-    [-1, 1],
-    [0, 1],
-    [1, 1],
-    [-1, 0],
-    [1, 0],
-    [-1, -1],
-    [0, -1],
-    [1, -1],
-  ];
-
-  const nCoord = coords.split(',').map((i) => parseInt(i));
-  const neighboringCoords: Coordinate[] = [];
-  for (const neighbor of relNeighbors) {
-    neighboringCoords.push(
-      `${neighbor[0] + nCoord[0]},${neighbor[1] + nCoord[1]}`
-    );
-  }
-  return neighboringCoords;
-}
-
-function tick(activeCells: CellMap) {
-  const sortedCells = new Map(
-    [...activeCells].sort((pairA, pairB) => {
-      const pair1 = pairA[0].split(',').map((i) => parseInt(i));
-      const pair2: number[] = pairB[0].split(',').map((i) => parseInt(i));
-      return pair1[1] - pair2[1] || pair1[0] - pair2[0];
-    })
-  );
-  console.log(sortedCells);
-
-  for (const cell of sortedCells) {
-    const neighbors = getNeighbors(cell[0]);
-    console.log(neighbors);
-
-    let activeNeighborCount = 0;
-    for (const neighbor of neighbors) {
-      if (activeCells.get(neighbor)) {
-        activeNeighborCount += 1;
-      }
+function getNeighbors(x: number, y: number) {
+    const neighbors: number[][] = [];
+    for (let i = x - 1; i <= x + 1; i++) {
+        for (let j = y - 1; j <= y + 1; j++) {
+            if (i === x && j === y) {
+                continue;
+            }
+            neighbors.push([i, j]);
+        }
     }
-
-    console.log(activeNeighborCount);
-  }
+    return neighbors;
 }
 
-tick(testCells);
+
+export function createNextFrame(currentGrid: number[][]) {
+    const nextGrid: number[][] = [];
+    for (let i = 0; i < currentGrid.length; i++ ) {
+        const newRow: number[] = [];
+        for (let j = 0; j < currentGrid[i].length; j++) {
+            const cell = currentGrid[i][j];
+            const neighbors = getNeighbors(i, j).filter(([x, y]) => x >= 0 && x < currentGrid.length && y >= 0 && y < currentGrid.length);
+            let aliveCount = 0;
+            for (const [x, y] of neighbors) {
+              const neighbor = currentGrid[x][y];
+              if (neighbor === 1) {
+                aliveCount += 1;
+              }
+            }
+            if (cell === 1 && aliveCount < 2) {
+                newRow.push(0);
+              } else if (cell === 1 && aliveCount >= 2 && aliveCount <= 3) {
+                newRow.push(1);
+              } else if (cell === 1 && aliveCount > 3) {
+                newRow.push(0);
+              } else if (cell === 0 && aliveCount === 3) {
+                newRow.push(1);
+              } else {
+                newRow.push(cell);
+              }
+        }
+        nextGrid.push(newRow)
+    }
+    return nextGrid;
+}
